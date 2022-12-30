448f76c5f29b994db7e49eb182d99534af389af6
Ken <kenalviola@gmail.com>
2022-12-30 15:54:25 -0600

    1M line script updated for loop

--- numbers.sh
+++ numbers.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
+SECONDS=0
+
+echo -n 'Start time: ' 
+date
+
 rm file1.txt
-for ((i=0; i<=999; i++))
+
+for ((i=0; i<=999999; i++))
 do
     echo $RANDOM >> file1.txt    
 done
+
+echo -n 'End time: '
+date
+
+echo "Runtime: $SECONDS seconds"
